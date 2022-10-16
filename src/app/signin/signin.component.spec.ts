c0b4b19dea5088f76ea400655e3ed7110403ca1f
JUBINBIJU <60598332+JUBINBIJU@users.noreply.github.com>
2022-10-16 09:36:04 +0530

    Update signin.component.spec.ts

--- src/app/signin/signin.component.spec.ts
+++ src/app/signin/signin.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
+import {Router} from '@angular/router'
 
-import { SigninComponent } from './signin.component';
 
-describe('SigninComponent', () => {
-  let component: SigninComponent;
-  let fixture: ComponentFixture<SigninComponent>;
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+  signform!:FormGroup
+  constructor(private formbuilder:FormBuilder,private http:HttpClient,private router:Router) { }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SigninComponent ]
-    })
-    .compileComponents();
+  ngOnInit(): void {
+    this.signform=this.formbuilder.group(
+      {
+        email:[''],
+        pwd:['']
 
-    fixture = TestBed.createComponent(SigninComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      }
+    )
+  }
+  signin(){
+    this.http.get<any>('http://localhost:3000/user').subscribe(res=>
+     {const user=res.find((a:any)=>{
+      return a.email===this.signform.value.email&&a.pwd===this.signform.value.pwd
+     }
+     )
+     if(user){
+      alert("user exist");
+    }
+    else
+    {
+      alert("doesnt exist")
+    }
+   
+  }) 
+     
+  }
+  
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+}
